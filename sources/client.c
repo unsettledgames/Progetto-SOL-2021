44d01ab5a045f710b1beb0a9b27fe0a6b63a57e4
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-05-07 19:12:06 +0200

    Terminato parsing degli argomenti
    Argomenti che devono essere correttamente validati dalle funzioni che li utilizzano.

--- sources/client.c
+++ sources/client.c
@@ -14,6 +14,7 @@ int main(int argc, char** argv)
     print_list(requests, "Requests");
 
     hashmap_clean(config);
+    list_clean(requests);
     
     return 0;
 }
@@ -22,6 +23,8 @@ int main(int argc, char** argv)
 void parse_options(Hashmap* config, List* requests, int n_args, char** args)
 {
     int opt;
+    unsigned short int used_name = FALSE;
+    unsigned short int used_val = FALSE;
     
     char* opt_name = malloc(sizeof(char) * OPT_NAME_LENGTH);
     char* opt_value = malloc(sizeof(char) * OPT_VALUE_LENGTH);
@@ -35,7 +38,7 @@ void parse_options(Hashmap* config, List* requests, int n_args, char** args)
     opt_value = malloc(sizeof(char) * OPT_VALUE_LENGTH);
     opt_name = malloc(sizeof(char) * OPT_NAME_LENGTH);
 
-    while ((opt = getopt(n_args, args, "hf:w:W:D:R:r:d:t:l:u:c:p")) != -1)
+    while ((opt = getopt(n_args, args, ":phf:w:W:D:R::r:d:t:l:u:c:")) != -1)
     {
         switch (opt)
         {
@@ -45,20 +48,25 @@ void parse_options(Hashmap* config, List* requests, int n_args, char** args)
             case 'd':
             case 't':
                 // Converto il nome dell'opzione a stringa, aggiungo i terminatori
-                sprintf(opt_name, "%c%c", opt, '\0');
-                sprintf(opt_value, "%s%c", optarg, '\0');
+                sprintf(opt_name, "%c", opt);
+                sprintf(opt_value, "%s", optarg);
                 // Uso tale stringa come chiave per il valore dell'argomento
                 hashmap_put(config, (void*)opt_value, opt_name);
 
-                // Rialloco la chiave così la prossima volta è in una locazione diversa
-                opt_name = malloc(sizeof(char) * 5);
-                // Rialloco il parametro così la prossima volta è in una locazione diversa
-                opt_value = malloc(sizeof(char) * 100);
+                used_name = TRUE;
+                used_val = TRUE;
                 break;
             case 'h':
                 print_client_options();
                 break;
             case 'p':
+                sprintf(opt_name, "p");
+                sprintf(opt_value, "1");
+                // Uso tale stringa come chiave per il valore dell'argomento
+                hashmap_put(config, (void*)opt_value, opt_name);
+
+                used_name = TRUE;
+                used_val = TRUE;
                 break;
             // Gestisco le opzioni che richiedono una lista di argomenti separati da virgola
             case 'w':
@@ -70,34 +78,44 @@ void parse_options(Hashmap* config, List* requests, int n_args, char** args)
                 // Salvo le informazioni passate da linea di comando
                 curr_request->code = opt;
                 curr_request->arguments = (char*)optarg;
-
-                printf("Parsing Op: %c, args: %s\n", curr_request->code, curr_request->arguments);
+                
                 // Inserisco la richiesta nella coda delle richieste
                 list_enqueue(requests, (void*)curr_request, NULL);
-
+                // Rialloco la struttura
                 curr_request = malloc(sizeof(Request));
                 break;
-            case 'R':
-                break;
             // Gestisco le istruzioni con un parametro opzionale
+            case 'R':
+                // Controllo se R ha un parametro o no
+                if (optarg != NULL)
+                    sprintf(opt_value, "%s", optarg);
+                else
+                    sprintf(opt_value, "-1");
+
+                curr_request->code = 'R';
+                curr_request->arguments = opt_value;
+                // Metto R in coda come operazione
+                list_enqueue(requests, (void*)curr_request, NULL);
+                curr_request = malloc(sizeof(Request));
+
+                used_val = TRUE;
+                break;
             case ':':
-                switch (optopt)
-                {
-                    case 'R':
-                        // Stampo il valore di default a stringa
-                        sprintf(opt_value, "%d", 0);
-                        // Metto quella stringa nella tabella
-                        //hashmap_put(ret, (void*)opt_value, "R");
-                        break;
-                    default:
-                        fprintf(stderr, "L'opzione %c non ammette argomenti opzionali.\n", optopt);
-                }
+                fprintf(stderr, "L'opzione %c necessita di un parametro.\n", optopt);
+                break;
             case '?':
-                fprintf(stderr, "L'opzione %c non e' stata immessa correttamente.\n", optopt);
+                fprintf(stderr, "L'opzione %c non e' riconosciuta.\n", optopt);
                 break;
             default:
                 break;
         }
+
+        if (used_name)
+            // Rialloco la chiave così la prossima volta è in una locazione diversa
+            opt_name = malloc(sizeof(char) * OPT_NAME_LENGTH);
+        if (used_val)
+            // Rialloco il parametro così la prossima volta è in una locazione diversa
+            opt_value = malloc(sizeof(char) * OPT_VALUE_LENGTH);
     }
 
     free(opt_name);
