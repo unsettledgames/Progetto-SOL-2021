06b5c4a71f711e84d512a62157e8f1625c6fb608
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2021-05-30 18:52:15 +0200

    Aggiornati test

--- scripts/test2.sh
+++ scripts/test2.sh
@@ -5,9 +5,10 @@ echo -e "4\n1000000\n10\nLSOfilestorage.sk\nLogs" > config.txt
 
 ./server config.txt &
 pid=$$
+sleep 1s
 for i in {1..11}; do
     ./client -f LSOfilestorage.sk -W TestDir/lorem$i.txt -p -D ToWriteIn &
 done
-sleep 2s
+sleep 1s
 kill -SIGHUP $pid &
 ./scripts/stats.sh
